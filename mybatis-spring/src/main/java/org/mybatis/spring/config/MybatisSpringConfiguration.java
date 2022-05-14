ac45066f1ac898dda4b93e0a42a20bcee2f5bdc2
feichaoyu <979331856@qq.com>
2022-05-14 12:04:21 +0800

    mybatis-spring

--- mybatis-spring/src/main/java/org/mybatis/spring/config/MybatisSpringConfiguration.java
+++ mybatis-spring/src/main/java/org/mybatis/spring/config/MybatisSpringConfiguration.java
@@ -1,21 +1,44 @@
 package org.mybatis.spring.config;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
-import java.io.Reader;
+import javax.sql.DataSource;
 
 @Configuration
 @MapperScan("org.mybatis.spring.mapper")
 public class MybatisSpringConfiguration {
 
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory() throws Exception {
+//        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+//        return new SqlSessionFactoryBuilder().build(reader);
+//    }
+
     @Bean
-    public SqlSessionFactory sqlSessionFactory() throws Exception {
-        Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-        return new SqlSessionFactoryBuilder().build(reader);
+    public SqlSessionFactoryBean sqlSessionFactoryBean(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource[] resources = resolver.getResources("org/mybatis/spring/mapper/*.xml");
+        sqlSessionFactoryBean.setMapperLocations(resources);
+        sqlSessionFactoryBean.setTypeAliasesPackage("org.mybatis.spring.model");
+        return sqlSessionFactoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        UnpooledDataSource dataSource = new UnpooledDataSource();
+        // 可以从配置文件取
+        dataSource.setDriver("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/play-with-mybatis");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345678");
+        return dataSource;
     }
 }
